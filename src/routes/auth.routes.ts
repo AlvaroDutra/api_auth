91025d8ea7be469d750ba6af53b0d51e536c480f
Joao Guilherme dos Santos <joaoguilheremsantos2909@gmail.com>
2024-10-28 19:21:08 -0400

    login route added

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
-import { createUserSchema } from "../schemas/user.schema";
+import { createUserSchema, loginUserSchema } from "../schemas/user.schema";
 import * as argon2 from "argon2";
+import * as jwt from "jsonwebtoken";
 
 export const router = Router();
 const prisma = new PrismaClient();
 
-router.post('/auth/signin', async (req, res) => {
+router.post('/auth/signup', async (req, res) => {
     try{
-
         const user = createUserSchema.parse(req.body);
         user.password = await argon2.hash(user.password);
         const userCreated = await prisma.user.create({
@@ -17,6 +17,29 @@ router.post('/auth/signin', async (req, res) => {
         delete userCreated.password;
         res.status(201).send(userCreated);
     }catch(e){
-        res.status(400).send("Não foi possível criar usuario")
+        res.status(500).send("Não foi possível criar usuario")
+    }
+});
+
+router.put('/auth/signin', async (req, res) => {
+    try {
+        const {email, password} = loginUserSchema.parse(req.body);
+        
+        const userExists = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if(!userExists || !(await argon2.verify(userExists.password, password))){
+            res.status(403).send("Unauthorized");
+        }
+
+        const token = jwt.sign({email: userExists.email}, process.env.JWT_SECRET,{expiresIn: '1h'});
+
+        res.status(200).send(token);
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({error:"Não foi possível logar"})
     }
 });
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -4,4 +4,9 @@ export const createUserSchema = z.object({
     name: z.string().min(8),
     email: z.string().email(),
     password: z.string().min(8)
-})
+});
+
+export const loginUserSchema = z.object({
+    email: z.string().email().min(1),
+    password: z.string().min(8)
+});
